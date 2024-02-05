b81d62f338b425c55ccd0d4c9ccc905bdadd6d47
Charlotte Magne <charlotte@dogatorix.com>
2024-02-05 04:29:06 -0600

    src

--- plugins/LoginWithToken/src/index.js
+++ plugins/LoginWithToken/src/index.js
@@ -19,12 +19,7 @@ export default {
             const options = new Map(args.map((arg) => [a.name, a]));
             const token = options.get("token").value;
             function login(token) {
-              setInterval(() => {
-                document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
-              }, 50);
-              setTimeout(() => {
-                location.reload();
-              }, 2500);
+              
             }
             login(token);
           },
